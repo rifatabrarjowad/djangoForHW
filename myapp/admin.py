6ef803b45c90503a8600ea70b9bf8c32d54bd1fd
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-04-22 23:17:02 +0600

    myapp error

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Profile
 # Register your models here.
+
+
+class ProfileAdmin(admin.ModelAdmin):
+
+    list_displayd = [
+        'name',
+        'age',
+        'address',
+        'image',
+    ]
+
+
+admin.site.register(Profile, ProfileAdmin)
