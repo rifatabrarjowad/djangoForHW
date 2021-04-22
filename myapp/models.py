1261de754685ec5e222ccdd45dbf1b36596a0fb8
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-04-22 22:48:58 +0600

    myapp migration

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    adress = models.TextField()
+    image = models.ImageField()
