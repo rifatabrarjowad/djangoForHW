7d144fd71904d7679b30d83f44a53bb5302b9370
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-04-29 02:27:31 +0600

    adding file

--- album/admin.py
+++ album/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Album
 # Register your models here.
+
+
+class AlbumAdmin(admin.ModelAdmin):
+
+    list_displayd = [
+        'description',
+        'thumbnail',
+        'creation',
+    ]
+
+
+admin.site.register(Album, AlbumAdmin)
+
